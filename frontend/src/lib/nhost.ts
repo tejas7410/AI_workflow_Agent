f25f25bf08de2dd27766ddf2fbe54623656eb59d
tejas7410 <tejaskangule7410509433@gmail.com>
2026-08-09 19:23:55 +0530

    feat: configure Nhost client

---
+++ frontend/src/lib/nhost.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@nhost/nhost-js";
+
+export const nhost = createClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN!,
+  region: process.env.NEXT_PUBLIC_NHOST_REGION!,
+});
